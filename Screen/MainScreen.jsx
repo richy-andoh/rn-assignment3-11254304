c020eca96449ebf26836d22e60270bd815a96d4a
richmond-andoh <richmondandoh00@gmail.com>
2024-05-31 16:30:38 +0000

    Safe area view setup

---
+++ Screen/MainScreen.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>MainScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({});
